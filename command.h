28d864c7ee9420b3e92db5afa6b2e521ebe8814b
wutianxing <wutianxing@h3d.com.cn>
2019-03-01 15:31:04 +0800

    command.h

---
+++ command.h
@@ -0,0 +1,15 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+#include "fileAndFolder.h"
+
+class Command
+{
+public:
+    Command();
+    ~Command();
+    void CommandWindows();
+    void HelpWord();
+    
+};
+
+#endif
